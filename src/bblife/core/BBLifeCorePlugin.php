1e5ccb20ef61e9ec917ea7e6d6c3c9a7cedd96c2
bbo51dog <bbo51dog@gmail.com>
2022-01-28 20:53:19 +0900

    Repository用Registryを削除

--- src/bblife/core/BBLifeCorePlugin.php
+++ src/bblife/core/BBLifeCorePlugin.php
@@ -2,8 +2,17 @@
 
 namespace bblife\core;
 
+use bblife\core\di\Container;
+use bblife\core\repository\ButlerUserRepository;
+use bblife\core\repository\UserRepository;
 use pocketmine\plugin\PluginBase;
 
 class BBLifeCorePlugin extends PluginBase {
 
+    private Container $container;
+
+    protected function onLoad(): void {
+        $this->container = (new Container())
+            ->define(UserRepository::class, ButlerUserRepository::class);
+    }
 }
