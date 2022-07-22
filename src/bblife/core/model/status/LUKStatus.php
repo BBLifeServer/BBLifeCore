41bcfca10f276671dbee9a6c73f1553234269345
Rark7040 <crimera.hello@gmail.com>
2022-07-23 00:17:52 +0900

    props

---
+++ src/bblife/core/model/status/LUKStatus.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace bblife\core\model\status;
+
+class LUKStatus implements IStatusProp{
+	public function getValue():int{
+		// TODO: Implement getValue() method.
+	}
+}
---
+++ src/bblife/core/model/status/STRStatus.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace bblife\core\model\status;
+
+class STRStatus implements IStatusProp{
+	public function getValue():int{
+		// TODO: Implement getValue() method.
+	}
+}
