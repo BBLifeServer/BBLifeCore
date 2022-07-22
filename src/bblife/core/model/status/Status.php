67c054fc6b0d06cafb5927eca84bceee2f01be7e
Rark7040 <crimera.hello@gmail.com>
2022-07-23 00:14:28 +0900

    props stub

--- src/bblife/core/model/status/Status.php
+++ src/bblife/core/model/status/Status.php
@@ -4,5 +4,9 @@ declare(strict_types=1);
 namespace bblife\core\model\status;
 
 class Status{
-
+	protected STRStatus $str;
+	protected VITStatus $vit;
+	protected INTStatus $int;
+	protected DEXStatus $dex;
+	protected LUKStatus $luk;
 }
